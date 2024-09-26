a90ca8f2ede7cc677f4efdb9250ccc47a92b328e
lapshuntanya <lapshun.tatiana@gmail.com>
2024-09-26 17:18:19 +0300

    add definition of PaidLesson

--- P34-T27-Inheritance/PaidLesson.cpp
+++ P34-T27-Inheritance/PaidLesson.cpp
@@ -1,1 +1,32 @@
 #include "PaidLesson.h"
+
+PaidLesson::PaidLesson(){
+    //Lesson() => C++, Inheritance, base class
+    price = 0;
+    cout << "PaidLesson()\n";
+}
+
+PaidLesson::PaidLesson(string theme, string info, string home, float price)
+    :Lesson(theme, info, home)
+{
+    //Lesson() => C++, Inheritance, base class
+    //this->theme = theme;
+    //this->info = info;
+    //setHome(home);
+
+    this->price = price;
+    cout << "PaidLesson(t, i, h, p)\n";
+}
+
+PaidLesson::~PaidLesson(){
+    cout << "~PaidLesson() => " << theme << endl;
+}
+
+void PaidLesson::setPrice(float price){ this->price = price; }
+float PaidLesson::getPrice() const{    return price;}
+
+void PaidLesson::showInfo() const{
+    //!!!!! showInfo(); => рекурсія
+    Lesson::showInfo(); // => theme, info, home
+    cout << "Price: " << price << "UAH\n\n";
+}
